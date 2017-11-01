98c8789f2583f07c671f4405ec97274a2a89a593
Vemund M Santi <vemund@santi.no>
2017-11-01 16:46:44 +0100

    feat: Add Category model. Show Category model in admin interface

--- data_models/models.py
+++ data_models/models.py
@@ -4,6 +4,23 @@ from django.utils.crypto import get_random_string
 from django.db import IntegrityError
 from datetime import datetime
 from django.utils.text import slugify
+from colorfield.fields import ColorField
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = "Kategori"
+        verbose_name_plural = "Kategorier"
+
+    name = models.CharField('Navn', max_length=64, unique=True)
+    text_color = ColorField('Tekstfarge', default='000000')
+    background_color = ColorField('Bakgrunnsfarge', default='ECB61C')
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return str(self.__unicode__())
 
 
 class Show(models.Model):
@@ -12,6 +29,11 @@ class Show(models.Model):
     image = models.ImageField(upload_to='uploads/images')
     lead = models.CharField(max_length=140)
     content = models.TextField()
+
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name='Kategorier'
+    )
 
     archived = models.BooleanField(default=False)
 
@@ -25,7 +47,9 @@ class Show(models.Model):
             try:
                 self.slug = slug
             except IntegrityError:
-                self.slug = '{slug}-{random_string}'.format(slug=slug, random_string=get_random_string(length=7))
+                self.slug = '{slug}-{random_string}'.format(
+                    slug=slug, random_string=get_random_string(length=7)
+                )
 
         super(Show, self).save(*args, **kwargs)
 
@@ -41,16 +65,33 @@ class Episode(models.Model):
     use_title = models.BooleanField(
         'Bruk tittel',
         default=False,
-        help_text='Vis tittelen på episoden. Om ikke tittel benyttes vises "Navn på show" + "Publiseringsdato"')
+        help_text='Vis tittelen på episoden. Om ikke tittel benyttes vises "Navn på show" + "Publiseringsdato"'
+    )
     lead = models.CharField('Beskrivelse', max_length=140)
 
-    show = models.ForeignKey(Show, related_name='episodes', verbose_name='Program')
+    show = models.ForeignKey(
+        Show,
+        related_name='episodes',
+        verbose_name='Program'
+    )
+
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name='Kategorier'
+    )
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, verbose_name='Opprettet av')
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        null=True,
+        verbose_name='Opprettet av'
+    )
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
-    on_demand_url = models.URLField(help_text="https://ondemandinfo.radiorevolt.no/")
+    on_demand_url = models.URLField(
+        help_text="https://ondemandinfo.radiorevolt.no/"
+    )
 
     def __unicode__(self):
         return self.title
@@ -69,10 +110,19 @@ class Post(models.Model):
 
     show = models.ForeignKey(Show, blank=True, null=True, related_name='posts')
 
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name='Kategorier'
+    )
+
     publish_at = models.DateTimeField(default=datetime.now)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='publications')
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='publications'
+    )
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -80,7 +130,9 @@ class Post(models.Model):
             try:
                 self.slug = slug
             except IntegrityError:
-                self.slug = '{slug}-{random_string}'.format(slug=slug, random_string=get_random_string(length=7))
+                self.slug = '{slug}-{random_string}'.format(
+                    slug=slug, random_string=get_random_string(length=7)
+                )
 
         super(Post, self).save(*args, **kwargs)
 
