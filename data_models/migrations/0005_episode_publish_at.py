9db63c0526ac7db0eaf6a97a6a3906d7a059d957
Christian Duvholt <christian@duvholt.net>
2017-11-05 02:24:12 +0100

    feat: use created_at to set publish_at when migrating

--- data_models/migrations/0005_episode_publish_at.py
+++ data_models/migrations/0005_episode_publish_at.py
@@ -6,6 +6,21 @@ import datetime
 from django.db import migrations, models
 
 
+def forwards_func(apps, schema_editor):
+    # We get the model from the versioned app registry;
+    # if we directly import it, it'll be the wrong version
+    Episode = apps.get_model("data_models", "episode")
+    db_alias = schema_editor.connection.alias
+    episodes = Episode.objects.using(db_alias).all()
+    for episode in episodes:
+        episode.publish_at = episode.created_at
+        episode.save()
+
+
+def reverse_func(apps, schema_editor):
+    pass
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -18,4 +33,5 @@ class Migration(migrations.Migration):
             name='publish_at',
             field=models.DateTimeField(default=datetime.datetime.now),
         ),
+        migrations.RunPython(forwards_func, reverse_func),
     ]
