6b07897e7ca92f9b8376af4f86f81e8568f55d08
Vemund Santi <vemund@santi.no>
2018-11-08 20:03:59 +0100

    refactor: use query string replacement

--- api_graphql/tests.py
+++ api_graphql/tests.py
@@ -11,33 +11,27 @@ def django_db_setup(django_db_setup, django_db_blocker):
         call_command('loaddata', 'test_fixtures.json')
 
 
+category_query = '''query {
+    MODEL_NAME {
+        id,
+        name,
+        textColor,
+        backgroundColor
+    }
+}'''
+
+
 @pytest.mark.django_db
 def test_category_by_id(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        category(id:1) {
-            id,
-            name,
-            textColor,
-            backgroundColor
-        }
-    }''')
-
+    executed = client.execute(category_query.replace('MODEL_NAME', 'category(id:1)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_categories(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allCategories {
-            id,
-            name,
-            textColor,
-            backgroundColor
-        }
-    }''')
-
+    executed = client.execute(category_query.replace('MODEL_NAME', 'allCategories'))
     snapshot.assert_match(executed)
 
 
@@ -52,507 +46,215 @@ def test_settings(snapshot):
             privacyPolicy,
         }
     }''')
-
     snapshot.assert_match(executed)
+
+
+show_query = '''query {
+    MODEL_NAME {
+        id
+        name
+        digasShowId
+        image
+        lead
+        content
+        categories {
+            id
+        }
+        slug
+        archived
+        createdAt
+        updatedAt
+        createdBy {
+            id
+        }
+        episodes {
+            id
+        }
+        posts {
+            id
+        }
+    }
+}'''
 
 
 @pytest.mark.django_db
 def test_show_by_id(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        show(id:1) {
-            id
-            name
-            digasShowId
-            image
-            lead
-            content
-            categories {
-                id
-            }
-            slug
-            archived
-            createdAt
-            updatedAt
-            createdBy {
-                id
-            }
-            episodes {
-                id
-            }
-            posts {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(show_query.replace('MODEL_NAME', 'show(id:1)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_show_by_slug(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        show(slug:"program2") {
-            id
-            name
-            digasShowId
-            image
-            lead
-            content
-            categories {
-                id
-            }
-            slug
-            archived
-            createdAt
-            updatedAt
-            createdBy {
-                id
-            }
-            episodes {
-                id
-            }
-            posts {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(show_query.replace('MODEL_NAME', 'show(slug:"program2")'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_shows(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-            allShows {
-                id
-                name
-                digasShowId
-                image
-                lead
-                content
-                categories {
-                    id
-                }
-                slug
-                archived
-                createdAt
-                updatedAt
-                createdBy {
-                    id
-                }
-                episodes {
-                    id
-                }
-                posts {
-                    id
-                }
-            }
-        }''')
-
+    executed = client.execute(show_query.replace('MODEL_NAME', 'allShows'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_shows_with_offset(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-            allShows(offset: 1) {
-                id
-                name
-                digasShowId
-                image
-                lead
-                content
-                categories {
-                    id
-                }
-                slug
-                archived
-                createdAt
-                updatedAt
-                createdBy {
-                    id
-                }
-                episodes {
-                    id
-                }
-                posts {
-                    id
-                }
-            }
-        }''')
-
+    executed = client.execute(show_query.replace('MODEL_NAME', 'allShows(offset: 1)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_shows_with_count(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-            allShows(offset: 2, count:2) {
-                id
-                name
-                digasShowId
-                image
-                lead
-                content
-                categories {
-                    id
-                }
-                slug
-                archived
-                createdAt
-                updatedAt
-                createdBy {
-                    id
-                }
-                episodes {
-                    id
-                }
-                posts {
-                    id
-                }
-            }
-        }''')
-
+    executed = client.execute(show_query.replace('MODEL_NAME', 'allShows(offset: 2, count:2)'))
     snapshot.assert_match(executed)
+
+
+episode_query = '''query {
+    MODEL_NAME {
+        id
+        title
+        lead
+        digasBroadcastId
+        digasShowId
+        categories {
+            id
+        }
+        show {
+            id
+        }
+        createdAt
+        updatedAt
+        publishAt
+        createdBy {
+            id
+        }
+        podcastUrl
+        onDemandUrl
+    }
+}'''
 
 
 @pytest.mark.django_db
 def test_episode_by_id(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        episode(id:1) {
-            id
-            title
-            lead
-            digasBroadcastId
-            digasShowId
-            categories {
-                id
-            }
-            show {
-                id
-            }
-            createdAt
-            updatedAt
-            publishAt
-            createdBy {
-                id
-            }
-            podcastUrl
-            onDemandUrl
-        }
-    }''')
-
+    executed = client.execute(episode_query.replace('MODEL_NAME', 'episode(id:1)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_episodes(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allEpisodes {
-            id
-            title
-            lead
-            digasBroadcastId
-            digasShowId
-            categories {
-                id
-            }
-            show {
-                id
-            }
-            createdAt
-            updatedAt
-            publishAt
-            createdBy {
-                id
-            }
-            podcastUrl
-            onDemandUrl
-        }
-    }''')
-
+    executed = client.execute(episode_query.replace('MODEL_NAME', 'allEpisodes'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_episodes_with_offset(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allEpisodes(offset:1) {
-            id
-            title
-            lead
-            digasBroadcastId
-            digasShowId
-            categories {
-                id
-            }
-            show {
-                id
-            }
-            createdAt
-            updatedAt
-            publishAt
-            createdBy {
-                id
-            }
-            podcastUrl
-            onDemandUrl
-        }
-    }''')
-
+    executed = client.execute(episode_query.replace('MODEL_NAME', 'allEpisodes(offset:1)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_episodes_with_count(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allEpisodes(offset:1,count:2) {
-            id
-            title
-            lead
-            digasBroadcastId
-            digasShowId
-            categories {
-                id
-            }
-            show {
-                id
-            }
-            createdAt
-            updatedAt
-            publishAt
-            createdBy {
-                id
-            }
-            podcastUrl
-            onDemandUrl
-        }
-    }''')
-
+    executed = client.execute(episode_query.replace('MODEL_NAME', 'allEpisodes(offset:1,count:2)'))
     snapshot.assert_match(executed)
+
+
+post_query = '''query {
+    MODEL_NAME {
+        id
+        title
+        slug
+        image
+        croppedImages {
+            large
+            medium
+            small
+            thumbnail
+        }
+        lead
+        content
+        categories {
+            id
+        }
+        deleted
+        episodes {
+            id
+        }
+        show {
+            id
+        }
+        publishAt
+        createdBy {
+            id
+        }
+    }
+}'''
 
 
 @pytest.mark.django_db
 def test_post_by_id(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        post(id:2) {
-            id
-            title
-            slug
-            image
-            croppedImages {
-                large
-                medium
-                small
-                thumbnail
-            }
-            lead
-            content
-            categories {
-                id
-            }
-            deleted
-            episodes {
-                id
-            }
-            show {
-                id
-            }
-            publishAt
-            createdBy {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(post_query.replace('MODEL_NAME', 'post(id:2)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_post_by_slug(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        post(slug:"den-forste-artikkelen") {
-            id
-            title
-            slug
-            image
-            croppedImages {
-                large
-                medium
-                small
-                thumbnail
-            }
-            lead
-            content
-            categories {
-                id
-            }
-            deleted
-            episodes {
-                id
-            }
-            show {
-                id
-            }
-            publishAt
-            createdBy {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(
+        post_query.replace('MODEL_NAME', 'post(slug:"den-forste-artikkelen")'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_posts(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allPosts {
-            id
-            title
-            slug
-            image
-            croppedImages {
-                large
-                medium
-                small
-                thumbnail
-            }
-            lead
-            content
-            categories {
-                id
-            }
-            deleted
-            episodes {
-                id
-            }
-            show {
-                id
-            }
-            publishAt
-            createdBy {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(post_query.replace('MODEL_NAME', 'allPosts'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_posts_with_offset(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allPosts(offset:3) {
-            id
-            title
-            slug
-            image
-            croppedImages {
-                large
-                medium
-                small
-                thumbnail
-            }
-            lead
-            content
-            categories {
-                id
-            }
-            deleted
-            episodes {
-                id
-            }
-            show {
-                id
-            }
-            publishAt
-            createdBy {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(post_query.replace('MODEL_NAME', 'allPosts(offset:3)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_posts_with_count(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allPosts(offset:3,count:5) {
-            id
-            title
-            slug
-            image
-            croppedImages {
-                large
-                medium
-                small
-                thumbnail
-            }
-            lead
-            content
-            categories {
-                id
-            }
-            deleted
-            episodes {
-                id
-            }
-            show {
-                id
-            }
-            publishAt
-            createdBy {
-                id
-            }
-        }
-    }''')
-
+    executed = client.execute(post_query.replace('MODEL_NAME', 'allPosts(offset:3,count:5)'))
     snapshot.assert_match(executed)
+
+
+user_query = '''query {
+    MODEL_NAME {
+        id
+        fullName
+        publications {
+            id
+        }
+    }
+}'''
 
 
 @pytest.mark.django_db
 def test_user_by_id(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        user(id:1) {
-            id
-            fullName
-            publications {
-                id
-            }
-        }
-    }''')
+    executed = client.execute(user_query.replace('MODEL_NAME', 'user(id:1)'))
     snapshot.assert_match(executed)
 
 
 @pytest.mark.django_db
 def test_all_users(snapshot):
     client = Client(schema)
-    executed = client.execute('''query {
-        allUsers {
-            id
-            fullName
-            publications {
-                id
-            }
-        }
-    }''')
+    executed = client.execute(user_query.replace('MODEL_NAME', 'allUsers'))
     snapshot.assert_match(executed)
 
 
